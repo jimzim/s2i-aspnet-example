7d245a20806fc8c7644238cd45986a9591281edc
Jim Zimmerman <jim@jimzimmerman.com>
2016-09-25 14:58:42 -0700

    test sql

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace WebApplication.Controllers
 {
@@ -10,6 +11,39 @@ namespace WebApplication.Controllers
     {
         public IActionResult Index()
         {
+
+            var connectionString = "Server=tcp:jzdotnetsql.database.windows.net,1433;Initial Catalog=jzdotnets;Persist Security Info=False;User ID=jzadmin;Password=jzSqlserver1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                var data = "Test";
+
+                try {
+
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        var queryString = "select * from Blog";
+                        SqlCommand command = new SqlCommand(queryString, connection);
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        try
+                        {
+                            while (reader.Read())
+                            {
+                                data += String.Format("{0}, {1}", reader["BlogId"], reader["Url"]);
+                                //Console.WriteLine(String.Format("{0}, {1}",
+                                // reader["BlogId"], reader["Url"]));
+                            }
+                        }
+                        catch(Exception ex)
+                        {
+                            // Console.WriteLine(ex.ToString());
+                            //return context.Response.WriteAsync("Error: " + ex.ToString());
+                        }
+                    }
+                    //return context.Response.WriteAsync("Hello from ASP.NET Core!" + data);
+
+                } catch(Exception ex) {
+                    //return context.Response.WriteAsync("Error: " + ex.ToString());
+                }
             return View();
         }
 
