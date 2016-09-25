c4099437ba22051bf7b07aa9a5c17d484a399419
Jim Zimmerman <jim@jimzimmerman.com>
2016-09-25 15:06:26 -0700

    test sql reference

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -12,38 +12,38 @@ namespace WebApplication.Controllers
         public IActionResult Index()
         {
 
-            var connectionString = "Server=tcp:jzdotnetsql.database.windows.net,1433;Initial Catalog=jzdotnets;Persist Security Info=False;User ID=jzadmin;Password=jzSqlserver1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-                var data = "Test";
+            // var connectionString = "Server=tcp:jzdotnetsql.database.windows.net,1433;Initial Catalog=jzdotnets;Persist Security Info=False;User ID=jzadmin;Password=jzSqlserver1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            //     var data = "Test";
 
-                try {
+            //     try {
 
-                    using (SqlConnection connection = new SqlConnection(connectionString))
-                    {
-                        var queryString = "select * from Blog";
-                        SqlCommand command = new SqlCommand(queryString, connection);
-                        connection.Open();
-                        SqlDataReader reader = command.ExecuteReader();
+            //         using (SqlConnection connection = new SqlConnection(connectionString))
+            //         {
+            //             var queryString = "select * from Blog";
+            //             SqlCommand command = new SqlCommand(queryString, connection);
+            //             connection.Open();
+            //             SqlDataReader reader = command.ExecuteReader();
 
-                        try
-                        {
-                            while (reader.Read())
-                            {
-                                data += String.Format("{0}, {1}", reader["BlogId"], reader["Url"]);
-                                //Console.WriteLine(String.Format("{0}, {1}",
-                                // reader["BlogId"], reader["Url"]));
-                            }
-                        }
-                        catch(Exception ex)
-                        {
-                            // Console.WriteLine(ex.ToString());
-                            //return context.Response.WriteAsync("Error: " + ex.ToString());
-                        }
-                    }
-                    //return context.Response.WriteAsync("Hello from ASP.NET Core!" + data);
+            //             try
+            //             {
+            //                 while (reader.Read())
+            //                 {
+            //                     data += String.Format("{0}, {1}", reader["BlogId"], reader["Url"]);
+            //                     //Console.WriteLine(String.Format("{0}, {1}",
+            //                     // reader["BlogId"], reader["Url"]));
+            //                 }
+            //             }
+            //             catch(Exception ex)
+            //             {
+            //                 // Console.WriteLine(ex.ToString());
+            //                 //return context.Response.WriteAsync("Error: " + ex.ToString());
+            //             }
+            //         }
+            //         //return context.Response.WriteAsync("Hello from ASP.NET Core!" + data);
 
-                } catch(Exception ex) {
-                    //return context.Response.WriteAsync("Error: " + ex.ToString());
-                }
+            //     } catch(Exception ex) {
+            //         //return context.Response.WriteAsync("Error: " + ex.ToString());
+            //     }
             return View();
         }
 
